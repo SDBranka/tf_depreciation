ad4126bd257dc6ad8b334d3885c6aa1830d7bf39
Sam Branka <sdbranka@me.com>
2023-05-04 19:27:46 -0400

    working on notes.py

--- _4_Machine_Learning/notes.py
+++ _4_Machine_Learning/notes.py
@@ -45,25 +45,101 @@ import pandas as pd
 # test/validation set
 
 
+# Model Evaluation
+# The key classification metrics we need to understand are:
+# - Accuracy
+# - Recall
+# - Precision
+# - F1-Score
+
+# Typically in any classification task your model can only achieve two results
+# - Either your model was correct in it's prediction
+# - Or your model was incorrect in it's prediction
+
+# Fortunately incorrect vs correct expands to situations where you have multiple
+# classes. For the purposes of explaining the metrics, let's imagine a binary
+# classification situation, where we only have two available classes.
+
+# In our example, we will attempt to predict if an image is a dog or a cat. 
+# Since this is supervised learning, we will first fit/train a model on 
+# training data, then test the model on testing data. Once we have the 
+# model's predictions from the X_test data, we compare it to the true y
+# values (the correct labels)
 
 
+# Accuracy
+# Accuracy in classification problems is the number of correct predictions made 
+# by the model divided by the total number of predictions
+# For example, if the x_test set was 100 images and our model correctly
+# predicted 80 images, then the accuracy is 0.8 or 80%
+# Accuracy is useful when target classes are well balanced
+# In our example, we would have roughly the same amound of cat images as we 
+# have dog images
+# Accuracy is not a good choice with unbalanced classes. Imagine we had 99
+# images of dogs and 1 image of a cat. If our model was simply a line that 
+# always predicted dog we would get 99% accuracy. In this situation we'll
+# want to understand recall and precision
 
 
+# Recall
+# Recall is the ability of a model to find all the relevant cases within a 
+# dataset. The precise definition of recall is the number of true positives
+# divided by the number of true positives plus the number of false negatives
 
 
+# Precision
+# Precision is the ability of a classification model to identify only the 
+# relevant data points. Precision is defined as the number of true positives
+# divided by the number of true positives plus the number of false positives
 
 
+# Recall and Precision
+# Often you have a trade-off between recall and precision. While recall 
+# expresses the ability to find all relevant instances in a dataset, precision
+# expresses the proportion of the data points our model says was relevant
+# that actually were relevant
 
 
+# F1-Score
+# In cases where we want to find an optimal blend of precision and recall we
+# can combine the two metrics using what is called the F1-Score.
+
+# The F1 score is the harmonic mean of precision and recall taking both 
+# metrics into account in the following equation:
+
+# F_1 = 2 * ((precision * recall) / (precision + recall))
+
+# The reason we use the harmonic mean instead of a simple average is because
+# it punishes extreme values. A classifier with a precision of 1.0 and a 
+# recall of 0.0 has a simple average of 0.5 but an F1-Score of 0.
+
+# We can also view all correctly classified versus incorrectly classified 
+# images in the form of a confusion matrix
 
 
+#                        _________________________________________________
+#                        |                                               |
+#                        |           Prediction Condition                |
+#           _____________|_______________________________________________|
+#           |            |                       |                       |
+#           | Total      |  Prediction Positive  |  Prediction Negative  |
+#           | Population |                       |                       |
+# __________|____________|_______________________|_______________________|
+#           |            |                       |                       |
+#           | Condition  |  True Positive (TP)   |  False Negative (FN)  |
+#           | Positve    |                       |    (Type II Error)    |
+# True      |____________|_______________________|_______________________|
+# Condition |            |                       |                       |
+#           | Condition  |  False Positive (FP)  |  True Negative (TN)   |
+#           | Negative   |   (Type I Error)      |                       |
+# __________|____________|_______________________|_______________________|
 
+# (see notes_3 for expanded chart)
 
-
-
-
-
-
+# The main point to remember with the confusion matrix and the various 
+# calculated metrics is that they are all fundamentally ways of comparing
+# the predicted values versus the true values. What constitutes "good" metrics
+# will really depend on the specific situation.
 
 
 
